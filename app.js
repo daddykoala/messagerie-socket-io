139a6eb60e0d86dd0c32e9e316e7805c32d73cde
daddykoala <moretticlement@outlook.fr>
2022-07-03 17:46:03 +0200

    install loader

--- app.js
+++ app.js
@@ -1,23 +1,83 @@
+//modules
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+const config = require('./config');
+const http = require('http');
+const socketio = require('socket.io');
 
+//variable globale
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+const port = config.express.port;
 let options = {
-    root : __dirname+'/views'
+    root: __dirname + '/views'
 }
+let usernames = [];
 
-app
+//middlewares
+app.use(express.static(options.root))
 app.use(morgan('dev'));
 
-app.get('/home',(req,res)=>{
+//route
+
+app.get('/home', (req, res) => {
     console.log(req.hostname);
-    res.sendFile('index.html',options);
+    res.sendFile('index.html', options);
 });
 
-app.get('/params/:name',(req,res)=>{
+app.get('/params/:name', (req, res) => {
     res.hello('req.params.name')
 });
 
-app.listen(3000,() => {
-    console.log(`Listening on http://localhost:3000/home`);
+//io
+io.on("connection", (socket) => {
+    console.log('user connected' + socket.id);
+
+    // setTimeout(() => {
+    //     socket.emit('hi', 'bonjour')
+    // }, 1000);
+
+    // socket.on('hi', (msg) => {
+    //     console.log(`client says ${msg}!`);
+    // })
+    //traitement
+    socket.on('setUsername', (userNameLog) => {
+
+        //traitemement de la chaine de caractere 
+        userNameLog = userNameLog.trim();
+
+        //verification de l'unicité de l'user
+        let userNameExist = false;
+
+        usernames.map((userName) => {
+            if (userName == userNameLog);
+            userNameExist = true;
+        })
+        //traitement final
+        if (userNameExist){
+
+            console.log('rejet');
+            socket.emit('rejectUsername');
+           
+
+        }else {
+
+            console.log('accepter');
+            usernames.push(userNameLog);
+            socket.emit('acceptUsername')
+        }
+          console.table(usernames)  
+
+
+    })
+
+    socket.on('disconnect', () => {
+        console.log('disconnected' + socket.id);
+
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/home`);
 });
